db3d48c6b4f8c32ab001f3768d9d29bc95699231
Jensooon <jenson.wintle@gmail.com>
2024-01-05 20:31:54 +0000

    Implemented swapping background themes. Need to implment the chaning of components.

--- CalendarApp/ViewModels/MainWindowViewModel.cs
+++ CalendarApp/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using CalendarApp.Themes;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace CalendarApp.ViewModels
 {
@@ -11,9 +13,31 @@ namespace CalendarApp.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        private string currentTheme;
+        public string CurrentTheme
+        {
+            get { return currentTheme; }
+            set { SetProperty(ref currentTheme, value); }
+        }
+
+        public DelegateCommand ToggleLightThemeCommand { get; set; }
+        public DelegateCommand ToggleDarkThemeCommand { get; set; }
+
         public MainWindowViewModel()
         {
+            CurrentTheme = "LightTheme";
 
+            ToggleLightThemeCommand = new DelegateCommand(ToggleLightTheme);
+            ToggleDarkThemeCommand = new DelegateCommand(ToggleDarkTheme);
+        }
+
+        private void ToggleLightTheme()
+        {
+        ThemesController.SetTheme(ThemesController.ThemeTypes.Light);
+        }
+        private void ToggleDarkTheme()
+        {
+        ThemesController.SetTheme(ThemesController.ThemeTypes.Dark);
         }
     }
 }
